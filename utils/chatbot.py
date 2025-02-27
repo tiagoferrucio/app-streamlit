f9830725f03cd48077c001925651b918647b0bb7
tiagoferrucio <38772077+tiagoferrucio@users.noreply.github.com>
2025-02-26 22:58:11 -0300

    Add files via upload

--- utils/chatbot.py
+++ utils/chatbot.py
@@ -7,7 +7,6 @@ from langchain_community.embeddings import OCIGenAIEmbeddings
 from langchain_community.chat_models.oci_generative_ai import ChatOCIGenAI
 from langchain_community.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.schema.runnable import RunnablePassthrough
@@ -15,10 +14,10 @@ from langchain.schema.output_parser import StrOutputParser
 
 def load_llm(config):
     chat_model = ChatOCIGenAI(
-        model_id="cohere.command-r-plus", ## - cohere.command-r-plus, cohere.command-r-16k
+        model_id="cohere.command-r-16k", ## - cohere.command-r-plus, cohere.command-r-16k
         service_endpoint="https://inference.generativeai.sa-saopaulo-1.oci.oraclecloud.com",
         compartment_id='ocid1.compartment.oc1..aaaaaaaa7ni42qbrptng34mhf7i3e23lmxes734ovsggllryxrofxkgk6gda',
-        model_kwargs={"temperature": 0.5, "max_tokens": 2000},
+        model_kwargs={"temperature": 0.7, "max_tokens": 2000},
     )
     
     return chat_model
@@ -41,41 +40,31 @@ def create_vectorstore(chunks,config):
 def create_conversation_chain(user_question, vectorstore):
     config = text.load_oci_config()
 
-    ##retriever = vectorstore.as_retriever()
-    #result = vectorstore.similarity_search_with_score(user_question, k=30)
-    #context = ' '.join([res[0].page_content for res in result])
+    llm = load_llm(config)
 
-    template = """
-    Você é um chatbot projetado para auxiliar os usuários.
+    ##retriever = vectorstore.as_retriever()
+    result = vectorstore.similarity_search_with_score(user_question, k=30)
+    context = ' '.join([res[0].page_content for res in result])
+
+    template = '''
+    Você é um acessor juridico e precisa ajudar o time de vendas exclarecendo algumas dúvidas;
+
     Responda em português;
     Use palavras que sua audiência vai entender;
     Siga um estilo para o seu texto; Evite palavras desnecessárias; Não seja redundante;
     Prefira discurso direto; Organize a informação por ordem de importância e uso;
-    Se o texto não contiver a resposta, responder que a resposta não está disponível.
-    Mantenha as respostas precisas para a pergunta
-    Responda apenas às perguntas com base no contexto fornecido abaixo:
+    Caso não saiba, não invente, diga que não sabe
+    Responda somente baseado no contexto abaixo;
 
     contexto: {context}
     
-    Pergunta: {question};
-    """
+    Pergunta: {user_question};
+    '''
     prompt = PromptTemplate(
         input_variables=["context", "user_question"],
         template=template
     )
-    chain_type_kwargs = {"prompt" : prompt}
 
-    llm = load_llm(config)
-    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    response = (prompt | llm).invoke({"context": context, "user_question": user_question})
 
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm = llm,
-        retriever= vectorstore.as_retriever(),
-        memory = memory,
-        combine_docs_chain_kwargs=chain_type_kwargs
-    )   
-
-    response  = conversation_chain.invoke({"question": user_question})
-    #response = (prompt | llm).invoke({"context": context, "user_question": user_question})
-
-    return(response.get("answer"))
+    return response.content
